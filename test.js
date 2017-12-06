2a96b41c7ab201de3afde689d243251251b28084
Dmitry <df.creative@gmail.com>
2017-12-06 14:35:48 -0500

    Add extra test

--- test.js
+++ test.js
@@ -1,41 +1,53 @@
 'use strict';
 
-const play = require('./');
-const lena = require('audio-lena/buffer');
-const AudioBuffer = require('audio-buffer');
+const play = require('./')
+const lena = require('audio-lena/buffer')
+const AudioBuffer = require('audio-buffer')
 const assert = require('assert')
 const context = require('audio-context')()
-
-let samples = new Float32Array(lena)
-let buf = new AudioBuffer(context, {numberOfChannels: 1, length: samples.length});
-buf.getChannelData(0).set(samples)
-
-let playback = play(buf, {
-	start: 1.2,
-	end: 1.29,
-	volume: .5,
-	loop: true,
-	rate: 2
-}, () => {
-	console.log('end');
-});
+const t = require('tape')
+const load = require('audio-loader')
 
 
-//TODO
-// setInterval(() => {
-// 	playback.rate *= 1.5
-// }, 10)
+t('lena', t => {
+	let samples = new Float32Array(lena)
+	let buf = new AudioBuffer(context, {numberOfChannels: 1, length: samples.length});
+	buf.getChannelData(0).set(samples)
 
-setTimeout(() => {
-	let play = playback.pause();
+	let playback = play(buf, {
+		start: 1.2,
+		end: 1.29,
+		volume: .5,
+		loop: true,
+		rate: 2
+	}, () => {
+		console.log('end');
+	});
 
 
-	assert(playback.currentTime);
+	//TODO
+	// setInterval(() => {
+	// 	playback.rate *= 1.5
+	// }, 10)
 
 	setTimeout(() => {
-		playback.play();
+		let play = playback.pause();
+
+		assert(playback.currentTime);
+
+		setTimeout(() => {
+			playback.play();
+		}, 500);
+		setTimeout(() => {
+			playback.pause();
+
+			t.end()
+		}, 1000);
 	}, 500);
-	setTimeout(() => {
-		playback.pause();
-	}, 1000);
-}, 500);
+})
+
+t('long sample file', t => {
+	load('./soundtest.wav', (err, buf) => {
+		play(buf)
+	})
+})
